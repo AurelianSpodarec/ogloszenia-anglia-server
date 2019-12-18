96cbe2f65bf67ae9ab1995de7896559cfebfcb7e
Aurelian <aurelians@libero.it>
2019-12-18 23:09:40 +0100

    Fixed typo for sendEmail

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -7,7 +7,7 @@ const sendEmail = async options => {
         part: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USERNAME,
-            password: process.env.EMAIL_PASSWORD
+            pass: process.env.EMAIL_PASSWORD
         }
     });
 
