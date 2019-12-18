ec3dd55dc4fd5ff456661e8dbe9edfa3a887c379
Aurelian <aurelians@libero.it>
2019-12-18 22:25:17 +0100

    Setting up fake email trap

---
+++ src/controllers/email.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = options => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            password: process.env.EMAIL_PASSWORD
+        }
+        // Activate in gmail 'less secure app' option
+    })
+}
