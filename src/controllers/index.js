8c86555ae550c88348c0678b12cd4cf88e4d331c
Aurelian <aurelians@libero.it>
2019-12-20 23:53:26 +0100

    Update

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 const userController = require('./userController');
 const homeController = require('./category/homeController');
 const carController = require('./category/carController');
+// const authController = require('./authController');
 
 module.exports = {
     userController,
