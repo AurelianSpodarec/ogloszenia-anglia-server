33cddd2974cbb4531bebc7060d2ecaa432914258
Aurelian <aurelians@libero.it>
2019-12-20 18:46:48 +0100

    Fixed few issues and finished password reset functionality

--- src/services/user/UserModel.js
+++ src/services/user/UserModel.js
@@ -68,12 +68,18 @@ const userSchema = new mongoose.Schema({
 //     return false;
 // }
 
-// userSchema.methods.pre('save', async function(next) {
-//     if(!this.isModified('password')) return next();
-//     this.password = await bcrypt.hash(this.password, 12);
-//     this.passwordConfirm= undefined;
-//     next();
-// })
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+})
+
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password') || this.isNew) return next();
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+})
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
